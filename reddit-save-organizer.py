f031521a5fff999350a36f0d9401b7902accfb15
theTaikun <43371347+theTaikun@users.noreply.github.com>
2019-01-29 22:56:15 -0500

    Initial commit

---
+++ reddit-save-organizer.py
@@ -0,0 +1,83 @@
+
+import secrets
+import json
+import requests
+import requests.auth
+import csv
+from time import sleep
+
+user=secrets.user
+passwd=secrets.passwd
+client=secrets.client
+secret=secrets.secret
+useragent=secrets.useragent
+
+final_useragent=useragent+" by /u/theTaikun"
+token =''
+
+def get_data_from_type(row):
+    if row['kind']=="t1" or row['kind']=="t3":
+        data=row['data']
+        if row['kind']=="t1":
+            return [data['subreddit'],data['link_title'].encode("utf-8"),data['author'],data['body'].encode("utf-8"),data['link_permalink'].encode('utf-8')]
+        elif row['kind']=="t3":
+            return [data['subreddit'],data['title'].encode("utf-8"),data['author'],data['selftext'].encode("utf-8"),data['url'].encode('utf-8')]
+
+def get_type_from_fullname(fullname):
+    prefix=fullname[:2]
+    if prefix == "t1":
+        return "Comment"
+    elif prefix == "t2":
+        return "Account"
+    elif prefix == "t3":
+        return "Link"
+    elif prefix == "t4":
+        return "Message"
+    elif prefix == "t5":
+        return "Subreddit"
+    elif prefix == "t6":
+        return "Award"
+    else:
+        return "ERROR"
+
+
+client_auth = requests.auth.HTTPBasicAuth(client, secret)
+post_data = {"grant_type": "password", "username": user, "password": passwd}
+headers = {"User-Agent":  final_useragent}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+auth_details=response.json()
+
+token=auth_details['access_token']
+headers = {"Authorization": "bearer "+token, "User-Agent":  final_useragent}
+
+after=""
+collection=[]
+pages=0
+saved=0
+with open("output.csv", 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(["subreddit", "title", "author", "body", "url"])
+    while (after is not None):
+        if pages==0:
+            query ="https://oauth.reddit.com/user/"+user+"/saved?limit=100"
+        else:
+            query ="https://oauth.reddit.com/user/"+user+"/saved?limit=100&after="+after
+        response = requests.get(query, headers=headers)
+        formatted_response=response.json()
+        #saved=response.json()
+        for entry in formatted_response["data"]["children"]:
+            data=entry['data']
+            collection.append(data)
+            
+            # May reseult in KeyError if field doesn't exist
+            spamwriter.writerow(get_data_from_type(entry))
+            print("{0} items processed".format(saved))
+            saved+=1
+        after=formatted_response["data"]["after"]
+        pages+=1
+        print("=================NEW PAGE================",pages)
+        sleep(1)
+print(collection)
+
+
+x=0
