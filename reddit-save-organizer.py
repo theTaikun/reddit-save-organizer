aeeeabec08f703c42845e611f760464b98da4e0e
theTaikun <43371347+theTaikun@users.noreply.github.com>
2019-01-31 17:24:50 -0500

    Large update that adds a lot of features.
    Creates several functions to download JSON data, combine into 1 JSON file, convert that file to CSV, then convert CSV to markdown. Could have skipped some steps, but I wanted to have multiple file types. JSON for archiving, CSV for organizing, MD for making it pretty.

--- reddit-save-organizer.py
+++ reddit-save-organizer.py
@@ -1,9 +1,15 @@
+"""
+TODO:
+1. Allow providing JSON instead of pulling from server ever time
+2. Format CSV into readable md/pdf/html
 
+"""
 import secrets
 import json
 import requests
 import requests.auth
 import csv
+import html
 from time import sleep
 
 user=secrets.user
@@ -12,16 +18,19 @@ client=secrets.client
 secret=secrets.secret
 useragent=secrets.useragent
 
+download=True
+json_pages=6
+
 final_useragent=useragent+" by /u/theTaikun"
 token =''
 
 def get_data_from_type(row):
-    if row['kind']=="t1" or row['kind']=="t3":
-        data=row['data']
-        if row['kind']=="t1":
-            return [data['subreddit'],data['link_title'],data['author'],data['body'],data['link_permalink']]
-        elif row['kind']=="t3":
-            return [data['subreddit'],data['title'],data['author'],data['selftext'],data['url']]
+   if row['kind']=="t1" or row['kind']=="t3":
+       data=row['data']
+       if row['kind']=="t1":
+          return [get_type_from_fullname(row['kind']),data['subreddit_name_prefixed'],data['link_title'],data['author'],data['body'],data['link_permalink'],data['link_permalink']]
+       elif row['kind']=="t3":
+          return [get_type_from_fullname(row['kind']),data['subreddit_name_prefixed'],data['title'],data['author'],data['selftext'],"https://www.reddit.com{0}".format(data['permalink']),data['url']]
 
 def get_type_from_fullname(fullname):
     prefix=fullname[:2]
@@ -40,44 +49,89 @@ def get_type_from_fullname(fullname):
     else:
         return "ERROR"
 
-
-client_auth = requests.auth.HTTPBasicAuth(client, secret)
-post_data = {"grant_type": "password", "username": user, "password": passwd}
-headers = {"User-Agent":  final_useragent}
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-auth_details=response.json()
-
-token=auth_details['access_token']
-headers = {"Authorization": "bearer "+token, "User-Agent":  final_useragent}
-
-after=""
-collection=[]
-pages=0
-saved=0
-with open("output.csv", 'w', newline='', encoding='utf-8') as csvfile:
-    spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(["subreddit", "title", "author", "body", "url"])
+def download_json():
+    client_auth = requests.auth.HTTPBasicAuth(client, secret)
+    post_data = {"grant_type": "password", "username": user, "password": passwd}
+    headers = {"User-Agent":  final_useragent}
+    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+    auth_details=response.json()
+    token=auth_details['access_token']
+    headers = {"Authorization": "bearer "+token, "User-Agent":  final_useragent}
+    
+    pages=0
+    after=""
+    
     while (after is not None):
         if pages==0:
             query ="https://oauth.reddit.com/user/"+user+"/saved?limit=100"
         else:
             query ="https://oauth.reddit.com/user/"+user+"/saved?limit=100&after="+after
         response = requests.get(query, headers=headers)
-        formatted_response=response.json()
-        #saved=response.json()
-        for entry in formatted_response["data"]["children"]:
-            data=entry['data']
-            collection.append(data)
-            
-            # May reseult in KeyError if field doesn't exist
-            spamwriter.writerow(get_data_from_type(entry))
-            print("{0} items processed".format(saved))
-            saved+=1
-        after=formatted_response["data"]["after"]
-        pages+=1
         print("=================NEW PAGE================",pages)
+        with open('raw_page-{0}.json'.format(pages), 'w') as outfile:  
+            json.dump(response.json(), outfile)
+        after=response.json()["data"]["after"]
+        pages+=1
+        
         sleep(1.001)
-print(collection)
+    json_pages=pages
 
+def combine_json():
+    collection={}
+    collection['data']=[]
+    for i in range(json_pages):
+        with open('raw_page-{0}.json'.format(i)) as json_file:  
+            print("Combining page {0}".format(i))
+            data = json.load(json_file)
+            entrynum=0
+            for entry in data['data']['children']:
+                collection['data'].append(entry)
+                entrynum+=1
+    with open('combined.json', 'a') as outfile:  
+        json.dump(collection, outfile)
+
+def write_csv():
+    with open("output.csv", 'w', newline='', encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(["post_type","subreddit", "title", "author", "body", "permalink","url"])
+
+        with open('combined.json') as json_file:  
+                data = json.load(json_file)
+                entrynum=0
+                for entry in data['data']:
+                    spamwriter.writerow(get_data_from_type(entry))
+                    #print("{0} items processed".format(saved))
+def UnicodeDictReader(utf8_data, **kwargs):
+    csv_reader = csv.DictReader(utf8_data, **kwargs)
+    for row in csv_reader:
+        yield {unicode(key, 'utf-8'):unicode(value, 'utf-8') for key, value in row.iteritems()}
+
+def create_markdown():
+    mdfile = open("output.md","w",encoding="utf-8")
+    with open('output.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        linenum=0
+        for row in reader:
+            if linenum>1:
+                url=row[6]
+                mdfile.write(
+"""
+# {0}
+## {1}
+{6} by _{2}_ ([Source]({3}))
+
+{4}
+[![{5}]({5}) ]({5})
+
+---
+""".format(row[2],row[1],row[3],row[5],row[4],url,row[0] if row[0] is "Comment" else "Post")
+                    )
+            linenum+=1
+
+download_json()
+combine_json()
+write_csv()
+
+create_markdown()
 
 x=0
